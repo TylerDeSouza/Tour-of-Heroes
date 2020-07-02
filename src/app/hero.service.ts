9645bcf330bc50a7fba9b1ea5a0f03f365978113
TylerDeSouza <tysouza@ufl.edu>
2020-07-02 00:15:22 -0400

    Refactored list to use service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../app/heroes/hero';
+import { HEROES } from '../app/heroes/mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 
   constructor() { }
 }
